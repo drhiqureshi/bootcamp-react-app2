e57d2ef44c15ec6ab3b18824d4bd72ad2948e41b
Haseeb Iqbal <44588344+drhiqureshi@users.noreply.github.com>
2020-06-13 14:30:39 +0500

    Initialize project React with Counter App useState Hook

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import {Message} from './message';
+import './App.css';
+
+export default function App() {
+  let [count,setCount] = useState(0);
+  let [isMorning,setMorning] = useState(true);
+  
+  return (
+    
+    <div className={`box ${isMorning ? 'daylight' : ''}`}> 
+    <h1>Day Time = {isMorning ? 'Morning' : 'Night'}</h1>    
+      <Message counter = { count }/>          
+      <br/>
+      <button onClick={ ()=> setCount(++count) }>Counter Increment</button>
+      <button onClick={ ()=> setCount(--count) }>Counter Decrement</button>
+      <br/><br/>
+      <button onClick={ ()=> setMorning(!isMorning) }>Update Day</button>
+      </div>
+      
+  );
+}
